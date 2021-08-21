946fd7651ed021d792d02f67d032cc108186d9d8
H34rtUnd3rB14d3 <k.maksim.e@yandex.ru>
2021-08-21 19:50:29 +0300

    Add comments

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,29 +1,29 @@
-def check_win(row, col):
-    symbol = field[row][col]
+def check_win(row: int, col: int) -> bool:  # function that check either 1p or 2p wins
+    sym = field[row][col]  # define whose turn
     win_con_row = 0
-    win_con_col = 0
+    win_con_col = 0  # if there is 3 same symbols in one row or column or diagonals
     win_con_diag = 0
 
-    if row == col:
+    if row == col:  # first diagonal check
         for i in range(1, ROW + 1):
-            if field[i][i] == symbol:
+            if field[i][i] == sym:
                 win_con_diag += 1
         if win_con_diag == 3:
             return True
         win_con_diag = 0
-    if ROW == row + col - 1:
+    if ROW == row + col - 1:  # second diagonal check
         for i in range(1, ROW + 1):
-            if field[i][ROW + 1 - i] == symbol:
+            if field[i][ROW + 1 - i] == sym:
                 win_con_diag += 1
         if win_con_diag == 3:
             return True
 
-    for i in range(1, ROW + 1):
-        if field[i][col] == symbol:
+    for i in range(1, ROW + 1):  # column check
+        if field[i][col] == sym:
             win_con_row += 1
 
-    for i in range(1, COL + 1):
-        if field[row][i] == symbol:
+    for i in range(1, COL + 1):  # row check
+        if field[row][i] == sym:
             win_con_col += 1
 
     if win_con_col == 3 or win_con_row == 3:
@@ -32,7 +32,7 @@ def check_win(row, col):
         return False
 
 
-def validate(data):
+def validate(data: list) -> str:  # function that validate user input and return message that describe errors
     if len(data) != 2:
         return "Incorrect data, try again"
     if not (data[0].isdigit() and data[1].isdigit()):
@@ -45,20 +45,20 @@ def validate(data):
     return ""
 
 
-def move():
+def move() -> tuple[int, int]:  # function that asks user to make move
     data = input("Make your move in format: row col ").split()
     message = validate(data)
-    while message:
+    while message:  # while we have error message repeat input
         data = input(message).split()
         message = validate(data)
     x, y = list(map(int, data))
-    x += 1
+    x += 1  # shift position by +1, +1 because of the field
     y += 1
 
     return x, y
 
 
-def show():
+def show():  # just draw field
     print(" | ".join(field[0]) + " |")
     print("-" * (ROW + 1) * 4)
     for i in range(1, ROW + 1):
@@ -66,18 +66,18 @@ def show():
         print("-" * (ROW + 1) * 4)
 
 
-ROW = COL = 3
-field = [[" " for _ in range(COL)] for _ in range(ROW)]
+ROW = COL = 3  # constants that define size of the field
+field = [[" " for _ in range(COL)] for _ in range(ROW)]  # fill not only playable part but also some decor
 field.insert(0, list(map(str, range(-1, COL))))
 field[0][0] = " "
 for i in range(ROW):
     field[i + 1].insert(0, str(i))
 
-move_counter = 1
-flag = " "
+move_counter = 1  # define move number
+flag = " "  # flag that define who is winner
 show()
-while move_counter != 10:
-    symbol = "X" if move_counter % 2 else "O"
+while move_counter != ROW * COL + 1:  # we have only row*col free space and if nobody wins that draw
+    symbol = "X" if move_counter % 2 else "O" # define whose turn
     x, y = move()
     if move_counter == 1:
         while x != ROW // 2 + 1 and y != COL // 2 + 1:
