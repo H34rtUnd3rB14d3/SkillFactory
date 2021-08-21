5258b196c9880bce8255ef7375ff3518bc84532c
H34rtUnd3rB14d3 <k.maksim.e@yandex.ru>
2021-08-21 12:49:35 +0300

    Added validate function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,11 +2,11 @@ def check(ch_row, ch_col):
     symbol = field[ch_row][ch_col]
     win_con_row = 0
     win_con_col = 0
-    for i in range(row):
+    for i in range(1, ROW + 1):
         if field[i][ch_col] == symbol:
             win_con_row += 1
 
-    for i in range(col):
+    for i in range(1, COL + 1):
         if field[ch_row][i] == symbol:
             win_con_col += 1
 
@@ -16,50 +16,70 @@ def check(ch_row, ch_col):
         return False
 
 
+def validate(data):
+    if len(data) != 2:
+        return "Incorrect data, try again"
+    if not (data[0].isdigit() and data[1].isdigit()):
+        return "Incorrect data, try again"
+    data = list(map(int, data))
+    if not (0 <= data[0] < ROW and 0 <= data[1] < COL):
+        return "Coordinates out of range"
+    if field[data[0] + 1][data[1] + 1] != " ":
+        return "Field is already taken"
+    return ""
+
+
 def move():
-    x, y = list(map(int, input("Enter your move in format: row col").split()))
-    while not (0 <= x < row and 0 <= y < col):
-        x, y = list(map(int, input("Incorrect data, try again").split()))
+    data = input("Make your move in format: row col ").split()
+    message = validate(data)
+    while message:
+        data = input(message).split()
+        message = validate(data)
+    x, y = list(map(int, data))
+    x += 1
+    y += 1
 
     return x, y
 
 
 def show():
     print(" | ".join(field[0]) + " |")
-    print("-" * (row + 1) * 4)
-    for i in range(1, row + 1):
+    print("-" * (ROW + 1) * 4)
+    for i in range(1, ROW + 1):
         print(" | ".join(field[i]) + " |")
-        print("-" * (row + 1) * 4)
+        print("-" * (ROW + 1) * 4)
 
 
-row = col = 3
-field = [[" " for _ in range(col)] for _ in range(row)]
-field.insert(0, list(map(str, range(-1, col))))
+ROW = COL = 3
+field = [[" " for _ in range(COL)] for _ in range(ROW)]
+field.insert(0, list(map(str, range(-1, COL))))
 field[0][0] = " "
-for i in range(row):
+for i in range(ROW):
     field[i + 1].insert(0, str(i))
 
 move_counter = 1
 flag = " "
 show()
-while True:
+while move_counter != 10:
     symbol = "X" if move_counter % 2 else "O"
     x, y = move()
     if move_counter == 1:
-        while x != row // 2 + 1 or y != col // 2 + 1:
+        while x != ROW // 2 + 1 and y != COL // 2 + 1:
             print("The first move should be at the center of a field")
             x, y = move()
+    field[x][y] = symbol
     if check(x, y):
         if symbol == "X":
             flag = "X"
         else:
             flag = "O"
         break
-    field[x][y] = symbol
     move_counter += 1
     show()
 
 if flag == "X":
     print("1p wins")
-else:
+elif flag == "O":
     print("2p wins")
+else:
+    print("draw")
